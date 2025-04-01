97d852a1cf12b8fd4a22636d554affed136ef3a9
CHAIK70 <andykanka@gmail.com>
2025-04-01 21:35:56 +0200

    Refactor form validation and error handling

--- portfolio-react/src/components/Form.tsx
+++ portfolio-react/src/components/Form.tsx
@@ -1,8 +1,10 @@
-import { useState, ChangeEvent, FormEvent } from 'react';
+// Form.tsx
+import { useState} from 'react';
 import { FaPaperPlane } from 'react-icons/fa';
+import FormField from './FormField';
+import { handleChange, handleSubmit } from '../utils/utils';
 
 export default function Form() {
-
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -18,141 +20,37 @@ export default function Form() {
     });
     const [formSubmitted, setFormSubmitted] = useState(false);
 
-    const validateField = (name: string, value: string) => {
-        let errorMessage = '';
-        if (name === 'name' && value.trim() === '') {
-            errorMessage = 'Name is required';
-        }
-        if (name === 'email') {
-            if (value.trim() === '') {
-                errorMessage = 'Email is required';
-            } else if (!/\S+@\S+\.\S+/.test(value)) {
-                errorMessage = 'Please enter a valid email';
-            }
-        }
-        if (name === 'message' && value.trim() === '') {
-            errorMessage = 'Message is required';
-        }
-        return errorMessage;
-    };
-
-    const validateForm = () => {
-        const newErrors = {
-            name: validateField('name', formData.name),
-            email: validateField('email', formData.email),
-            message: validateField('message', formData.message),
-        };
-        
-        setErrors(newErrors);
-        
-        const hasErrors = Object.values(newErrors).some(error => error !== '');
-        return !hasErrors;
-    };
-
-    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = e.target;
-        const updatedFormData = { ...formData, [name]: value };
-        setFormData(updatedFormData);
-
-        if (formSubmitted) {
-            const errorMessage = validateField(name, value);
-            setErrors({ ...errors, [name]: errorMessage });
-        }
-
-        const allFieldsHaveValue = 
-            updatedFormData.name.trim() !== '' &&
-            updatedFormData.email.trim() !== '' &&
-            updatedFormData.message.trim() !== '';
-            
-        setIsFormValid(allFieldsHaveValue);
-    };
-    
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        setFormSubmitted(true);
-        
-        const isValid = validateForm();
-        
-        if (isValid) {
-            setButtonText('Sending...');
-            
-            setTimeout(() => {
-                setButtonText('Message Sent!');
-                
-                // Resetea el formulario después de enviar
-                setFormData({ name: '', email: '', message: '' });
-                setIsFormValid(false);
-                setFormSubmitted(false);
-                
-                setTimeout(() => {
-                    setButtonText('Send');
-                }, 2000);
-            }, 3000);
-        }
-    };
-
-    const buttonBgClass = buttonText === 'Message Sent!' 
-        ? 'bg-green-600 shadow-green-600 scale-105'
-        : buttonText === 'Sending...'
-        ? 'bg-blue-600 shadow-[0px_0px_20px_rgba(37,99,235,1)] scale-105'
-        : isFormValid
-        ? 'bg-blue-600 cursor-pointer'
-        : 'bg-[var(--main-bg-color)] cursor-not-allowed';
-
     return (
         <div className="flex flex-col items-center justify-center w-full max-w-5xl p-16">
-            <form onSubmit={handleSubmit} className="flex flex-col gap-14 w-full max-w-4xl">
-                <div className='relative'>
-                    <input
-                        type="text"
-                        id="name"
-                        placeholder="Name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        name="name"
-                        className="w-full bg-[var(--main-bg-color)] text-white p-4 rounded-lg shadow-[0px_0px_20px_rgba(37,99,235,0.5)] 
-                            focus:outline-none focus:shadow-[0px_0px_20px_rgba(37,99,235,1)] transition-all ease-in-out duration-300"
-                    />
-                    {errors.name && (
-                        <div className="absolute text-red-500 text-sm mt-1">{errors.name}</div>
-                    )}
-                </div>
-
-                <div className='relative'>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        className="w-full bg-[var(--main-bg-color)] text-white p-4 rounded-lg shadow-[0px_0px_20px_rgba(37,99,235,0.5)] 
-                            focus:outline-none focus:shadow-[0px_0px_20px_rgba(37,99,235,1)] transition-all ease-in-out duration-300"
-                    />
-                    {errors.email && (
-                        <div className="absolute text-red-500 text-sm mt-1">{errors.email}</div>
-                    )}
-                </div>
-
-                <div className='relative'>
-                    <textarea
-                        id="message"
-                        name="message"
-                        placeholder="Write your message here..."
-                        value={formData.message}
-                        onChange={handleChange}
-                        className="w-full bg-[var(--main-bg-color)] text-white p-4 rounded-lg shadow-[0px_0px_20px_rgba(37,99,235,0.5)] 
-                            focus:outline-none focus:shadow-[0px_0px_20px_rgba(37,99,235,1)] transition-all ease-in-out duration-300 resize-none h-32"
-                    ></textarea>
-                    {errors.message && (
-                        <div className="absolute text-red-500 text-sm mt-1">{errors.message}</div>
-                    )}
-                </div>
-
+            <form onSubmit={(e) => handleSubmit(e, formData, setFormData, setButtonText, setIsFormValid, setFormSubmitted, setErrors)} className="flex flex-col gap-14 w-full max-w-4xl">
+                <FormField
+                    name="name"
+                    type="text"
+                    placeholder='Name'
+                    value={formData.name}
+                    error={errors.name}
+                    onChange={(e) => handleChange(e, formData, setFormData, errors, setErrors, formSubmitted, setIsFormValid)}
+                />
+                <FormField
+                    name="email"
+                    type="email"
+                    placeholder='Email'
+                    value={formData.email}
+                    error={errors.email}
+                    onChange={(e) => handleChange(e, formData, setFormData, errors, setErrors, formSubmitted, setIsFormValid)}
+                />
+                <FormField
+                    name="message"
+                    type="textarea"
+                    placeholder='Write your message here...'
+                    value={formData.message}
+                    error={errors.message}
+                    onChange={(e) => handleChange(e, formData, setFormData, errors, setErrors, formSubmitted, setIsFormValid)}
+                />
                 <button
                     type="submit"
                     className={`flex items-center justify-center w-full mx-auto font-bold text-white p-4 rounded-lg shadow-[0px_0px_20px_rgba(37,99,235,0.5)] max-w-sm 
-                        hover:shadow-[0px_0px_20px_rgba(37,99,235,1)] transition-all ease-in-out duration-300 hover:scale-105 ${buttonBgClass}`}
+                        hover:shadow-[0px_0px_20px_rgba(37,99,235,1)] transition-all ease-in-out duration-300 hover:scale-105 ${buttonText === 'Message Sent!' ? 'bg-green-600 shadow-green-600 scale-105' : buttonText === 'Sending...' ? 'bg-blue-600 shadow-[0px_0px_20px_rgba(37,99,235,1)] scale-105' : isFormValid ? 'bg-blue-600 cursor-pointer' : 'bg-[var(--main-bg-color)] cursor-not-allowed'}`}
                 >
                     {buttonText} <FaPaperPlane className="ml-2 rotate-45" />
                 </button>
--- portfolio-react/src/utils/utils.ts
+++ portfolio-react/src/utils/utils.ts
@@ -1,7 +1,97 @@
+import { Dispatch, FormEvent, SetStateAction, ChangeEvent } from "react"
 
-export const handleScroll = (id: string) => {
-    const section = document.getElementById(id);
-    if (section) {
-        section.scrollIntoView({ behavior: "smooth" });
+    export const handleScroll = (id: string) => {
+        const section = document.getElementById(id)
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" })
+        }
     }
-}
+
+    export const validateField = (name: string, value: string) => {
+        let errorMessage = '';
+        if (name === 'name' && value.trim() === '') {
+            errorMessage = 'Name is required'
+        }
+        if (name === 'email') {
+            if (value.trim() === '') {
+                errorMessage = 'Email is required'
+            } else if (!/\S+@\S+\.\S+/.test(value)) {
+                errorMessage = 'Please enter a valid email';
+            }
+        }
+        if (name === 'message' && value.trim() === '') {
+            errorMessage = 'Message is required';
+        }
+        return errorMessage;
+    };
+
+    export const validateForm = (formData: { name: string; email: string; message: string }, setErrors: Function) => {
+        const newErrors = {
+            name: validateField('name', formData.name),
+            email: validateField('email', formData.email),
+            message: validateField('message', formData.message),
+        };
+
+        setErrors(newErrors);
+
+        const hasErrors = Object.values(newErrors).some(error => error !== '');
+        return !hasErrors;
+    };
+
+    export const handleChange = (
+        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+        formData: { name: string; email: string; message: string },
+        setFormData: Dispatch<SetStateAction<{ name: string; email: string; message: string }>>,
+        errors: { name: string; email: string; message: string },
+        setErrors: Function,
+        formSubmitted: boolean,
+        setIsFormValid: Function
+    ) => {
+        const { name, value } = e.target;
+        const updatedFormData = { ...formData, [name]: value };
+        setFormData(updatedFormData);
+
+        if (formSubmitted) {
+            const errorMessage = validateField(name, value);
+            setErrors({ ...errors, [name]: errorMessage });
+        }
+
+        const allFieldsHaveValue =
+            updatedFormData.name.trim() !== '' &&
+            updatedFormData.email.trim() !== '' &&
+            updatedFormData.message.trim() !== '';
+
+        setIsFormValid(allFieldsHaveValue);
+    };
+
+    export const handleSubmit = (
+        e: FormEvent,
+        formData: { name: string; email: string; message: string },
+        setFormData: Dispatch<SetStateAction<{ name: string; email: string; message: string }>>,
+        setButtonText: Dispatch<SetStateAction<string>>,
+        setIsFormValid: Dispatch<SetStateAction<boolean>>,
+        setFormSubmitted: Dispatch<SetStateAction<boolean>>,
+        setErrors: Function
+    ) => {
+        e.preventDefault();
+        setFormSubmitted(true);
+
+        const isValid = validateForm(formData, setErrors);
+
+        if (isValid) {
+            setButtonText('Sending...');
+
+            setTimeout(() => {
+                setButtonText('Message Sent!');
+
+                // Resetea el formulario después de enviar
+                setFormData({ name: '', email: '', message: '' });
+                setIsFormValid(false);
+                setFormSubmitted(false);
+
+                setTimeout(() => {
+                    setButtonText('Send');
+                }, 2000);
+            }, 3000);
+        }
+    };
