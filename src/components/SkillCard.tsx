b00dd285b64fb57e084a1c06903a6f2b007c3862
CHAIK70 <andykanka@gmail.com>
2025-09-10 21:26:30 +0200

    style(layout): adjust element sizes for better layout consistency

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -9,7 +9,7 @@ type SkillsCardsProps = {
 
 const SkillCard: FC<SkillsCardsProps> = ({ title, icon: Icon, color }) => {
     return (
-        <div className="md:flex-1/4 bg-[var(--second-bg-color)] text-white p-6 rounded-2xl shadow-[0px_0px_10px_rgba(0,0,0,0.5)] transition-all ease-in-out duration-300 flex flex-col items-center w-24 md:w-40 md:h-32 justify-center
+        <div className="md:flex-1/4 bg-[var(--second-bg-color)] text-white p-6 rounded-2xl shadow-[0px_0px_10px_rgba(0,0,0,0.5)] transition-all ease-in-out duration-300 flex flex-col items-center w-32 md:w-40 md:h-32 justify-center
         hover:shadow-[0px_0px_20px_rgba(37,99,235,1)] hover:scale-105 border-2 border-[var(--main-bg-color)] cursor-pointer">
             <Icon className={`w-6 h-6 mb-4 ${color}`} />
             <h3 className="text-xm font-bold">{title}</h3>
--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <section id="home" className="w-full min-h-screen flex flex-col items-center justify-center text-center gap-y-6 px-6 scroll-mt-20">
             <div className="flex flex-col items-center justify-center gap-y-1 md:gap-y-4">
-                <h1 className="flex items-center justify-center text-2xl sm:text-5xl lg:text-6xl font-bold">
+                <h1 className="flex items-center justify-center text-xl sm:text-5xl lg:text-6xl font-bold">
                     <FontAwesomeIcon icon={faCode} className="text-[var(--main-color)] mr-3 sm:mr-4 lg:mr-6" />
                     Hello, I am Andy Álvarez
                     <FontAwesomeIcon icon={faCode} className="text-[var(--main-color)] ml-3 sm:ml-4 lg:ml-6" />
@@ -19,7 +19,7 @@ export default function Home() {
             <div className="mt-32">
                 <button 
                     onClick={() => handleScroll("contact")} 
-                    className="bg-[var(--main-color)] text-white text-xl sm:text-2xl font-bold uppercase tracking-wide 
+                    className="bg-[var(--main-color)] text-white text-xl md:text-2xl font-bold uppercase tracking-wide 
                     py-4 px-10 rounded-lg shadow-lg transition-all duration-300 ease-in-out 
                     hover:bg-blue-600 hover:shadow-[0px_0px_40px_rgba(37,99,235,1)] cursor-pointer hover:scale-105"
                 >
