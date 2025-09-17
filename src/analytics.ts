d5c74bb9124e8ba5d86172347c0f14806e7fbf4b
CHAIK70 <andykanka@gmail.com>
2025-09-18 00:32:31 +0200

    fix analytics typ error

--- src/analytics.ts
+++ src/analytics.ts
@@ -4,6 +4,6 @@ export const initGA = () => {
   ReactGA.initialize("G-NJPM7NYNGZ");
 };
 
-export const logPageView = (path) => {
+export const logPageView = (path: string) => {
   ReactGA.send({ hitType: "pageview", page: path });
 };
