595596258b244fec20017c1f4628290d43e99b2a
CHAIK70 <andykanka@gmail.com>
2025-10-08 10:50:38 +0200

    Update CV file

--- src/utils/handleDownload.ts
+++ src/utils/handleDownload.ts
@@ -1,8 +1,8 @@
 
 const handleDownload = (): void => {
     const link = document.createElement("a");
-    link.href = "/CV-Andy-Álvarez-Criado.pdf";
-    link.download = "CV-Andy-Álvarez-Criado.pdf";
+    link.href = "/CV_Andy_Álvarez_Criado.pdf";
+    link.download = "CV_Andy_Álvarez_Criado.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
