c9eea1cc409b7bd2d320477a43b6e16e1cf8dda5
CHAIK70 <andykanka@gmail.com>
2025-10-16 12:20:54 +0200

    Update Contact.tsx

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,21 +1,20 @@
-import SocialCard from "../components/SocialCard";
-import Form from "../components/Form";
-import { Reveal } from "../hooks/useScrollReveal";
+import SocialCard from '../components/SocialCard';
+import Form from '../components/Form';
+import { Reveal } from '../hooks/useScrollReveal';
 
 export default function Contact() {
-    return (
-        <section
-            id="contact"
-            className="flex flex-col min-h-screen items-center justify-center w-full"
-        >
-            
-            <div className="w-[80%] md:w-full">
-                <Reveal animation="fade-up">
-                    <h1 className="text-5xl font-bold mb-16 text-center">Contact</h1>
-                    <SocialCard />
-                    <Form />
-                </Reveal>
-            </div>
-        </section>
-    );
+  return (
+    <section
+      id="contact"
+      className="flex flex-col min-h-screen items-center justify-center w-full"
+    >
+      <div className="w-[80%] lg:w-full">
+        <Reveal animation="fade-up">
+          <h1 className="text-5xl font-bold mb-16 text-center">Contact</h1>
+          <SocialCard />
+          <Form />
+        </Reveal>
+      </div>
+    </section>
+  );
 }
