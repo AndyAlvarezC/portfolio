9ec1edbdf0bbb4b7459c473678c5aaa8a88139d8
CHAIK70 <andykanka@gmail.com>
2025-10-17 17:53:23 +0200

    Change language icon selector

--- src/components/ui/LanguageSelector.tsx
+++ src/components/ui/LanguageSelector.tsx
@@ -1,39 +1,57 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { MdOutlineLanguage } from 'react-icons/md';
 
 export const LanguageSelector = () => {
   const { i18n } = useTranslation();
   const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
 
   const changeLanguage = (lang: string) => {
     i18n.changeLanguage(lang);
     setOpen(false);
   };
 
+  const languages = [
+    { code: 'en', flag: '🇺🇸' },
+    { code: 'es', flag: '🇪🇸' },
+  ];
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="relative">
+    <div className="relative" ref={ref}>
       <button
         onClick={() => setOpen((prev) => !prev)}
-        className="text-xl cursor-pointer"
+        className="flex items-center justify-center text-xl cursor-pointer"
       >
-        🌐
+        <MdOutlineLanguage className='text-2xl'/>
       </button>
 
-      {/* Dropdown */}
       {open && (
-        <div className="absolute right-0 mt-2 bg-white text-black shadow-lg rounded p-2 flex flex-col">
-          <button
-            onClick={() => changeLanguage('en')}
-            className="hover:bg-gray-100 px-3 py-1 text-left cursor-pointer"
-          >
-            English
-          </button>
-          <button
-            onClick={() => changeLanguage('es')}
-            className="hover:bg-gray-100 px-3 py-1 text-left cursor-pointer"
-          >
-            Español
-          </button>
+        <div className="absolute left-1/2 top-full -translate-x-1/2 mt-2 bg-[var(--second-bg-color)] rounded-lg p-2 flex flex-col shadow-lg">
+          {languages.map((lang) => (
+            <button
+              key={lang.code}
+              onClick={() => changeLanguage(lang.code)}
+              className={`flex items-center px-3 py-1 cursor-pointer gap-2 rounded-md transition-colors duration-200 hover:bg-gray-700 ${
+                i18n.language === lang.code ? 'bg-blue-600 font-semibold' : ''
+              }`}
+            >
+              <span className="text-xl">{lang.flag}</span>
+            </button>
+          ))}
         </div>
       )}
     </div>
